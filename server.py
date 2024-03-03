a1d541011bf2115cf9e924b767a755a47e36cbae
russellandrady <russellsuper2000@gmail.com>
2024-03-03 21:56:27 +0530

    mostlydone

--- server.py
+++ server.py
@@ -20,12 +20,16 @@ def signin():
         email=request.form['email']
         pwd=request.form['password']
         cur = mysql.connection.cursor()
-        cur.execute(f"SELECT username,email,password,id FROM student WHERE email='{email}'")#find where is the email
+        cur.execute(f"SELECT username,email,password,id,sub1,sub2,sub3 FROM student WHERE email='{email}'")#find where is the email
         user = cur.fetchone()
         cur.close()
         if user and pwd ==user[2]:
             session['username']=user[0]
+            session['email']=user[1]
             session['id']=user[3]
+            session['sub1']=user[4]
+            session['sub2']=user[5]
+            session['sub3']=user[6]
             return redirect(url_for('profile',id=session['id']))
         else:
             return render_template('signin.html', error='invalid data')
@@ -49,6 +53,35 @@ def register():
 def profile(id):
     return render_template('profile.html')
 
+@app.route('/edit', methods=['GET', 'POST'])
+def edit():
+    success = False
+    if request.method == 'POST':
+        id = session['id']
+        username = request.form['username']
+        email = request.form['email']
+        sub1 = request.form['sub1']
+        sub2 = request.form['sub2']
+        sub3 = request.form['sub3']
+        cur = mysql.connection.cursor()
+        cur.execute("UPDATE student SET username=%s, email=%s, sub1=%s, sub2=%s, sub3=%s WHERE id=%s",
+                    (username, email, sub1, sub2, sub3, id))
+        mysql.connection.commit()
+        cur.close()
+        success = True
+        session['sub1']=sub1
+        session['sub2']=sub2
+        session['sub3']=sub3
+        session['username']=username
+        session['email']=email
+        return redirect(url_for('profile',id=id))
+    return render_template('profile.html', success=success)
+
+
+@app.route('/signout')
+def signout():
+    session.clear()
+    return redirect(url_for('home'))
 
 if  __name__ == '__main__':
     app.run(debug = True)
